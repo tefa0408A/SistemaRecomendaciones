a9e5835586679e365a284c2e164d9ddb55764375
tefa0408A <estefany.alfaro.rz@gmail.com>
2025-01-18 07:37:17 -0500

    estucture

---
+++ Recomendaciones-Backend/src/main/java/com/example/SistemaRecomendaciones/SistemaRecomendacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaRecomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRecomendacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRecomendacionesApplication.class, args);
+	}
+
+}
---
+++ Recomendaciones-Backend/src/test/java/com/example/SistemaRecomendaciones/SistemaRecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaRecomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRecomendacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
