cd9bf515b0306f63b250f668105c31ed680f1944
tefa0408A <estefany.alfaro.rz@gmail.com>
2025-02-02 09:50:22 -0500

    Microservicio de restaurante

--- Recomendaciones-Backend/MICRO-USER/src/main/java/com/example/SistemaRecomendaciones/service/JwtService.java
+++ Recomendaciones-Backend/MICRO-USER/src/main/java/com/example/SistemaRecomendaciones/service/JwtService.java
@@ -2,8 +2,11 @@ package com.example.SistemaRecomendaciones.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Map;
+
 public interface JwtService {
     String extractUsername(String token);
     String generateToken(UserDetails userDetails);
+    String generateRefreshToken(Map<String,Object> extraClaim,UserDetails userDetails);
     boolean validateToken(String token, UserDetails userDetails);
 }
