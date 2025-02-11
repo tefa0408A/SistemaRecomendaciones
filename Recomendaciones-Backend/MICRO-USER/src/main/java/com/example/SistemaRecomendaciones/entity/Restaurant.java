c2f03124d0cbafcb4939fa6bb193b49a3f3c0bbc
tefa0408A <estefany.alfaro.rz@gmail.com>
2025-02-11 17:37:54 -0500

    new fields

--- Recomendaciones-Backend/MICRO-USER/src/main/java/com/example/SistemaRecomendaciones/entity/Restaurant.java
+++ Recomendaciones-Backend/MICRO-USER/src/main/java/com/example/SistemaRecomendaciones/entity/Restaurant.java
@@ -12,6 +12,8 @@ public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private int calificacion;
+    private String distrito;
     private String nombre;
     private String imagenUrl;
     private String ubicacion;
